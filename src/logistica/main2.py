e1be56805b89d0f0cd5e2508464f71323677add5
A. Alfonso F. R. <alffore@gmail.com>
2024-07-05 00:41:53 -0600

    Se completa
    Se completa el desarrollo de la regresión logística

--- src/logistica/main2.py
+++ src/logistica/main2.py
@@ -1,27 +1,24 @@
-# https://www.youtube.com/watch?v=OGpQxIkR4ao
+# https://github.com/patrickloeber/pytorchTutorial
 
 import torch
 import torch.nn as nn
 import numpy as np
-
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-# data
-
+# 0) Prepare data
 bc = datasets.load_breast_cancer()
 X, y = bc.data, bc.target
 
 n_samples, n_features = X.shape
 
-print(n_samples, n_features)
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
+# scale
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
+X_test = sc.transform(X_test)
 
 X_train = torch.from_numpy(X_train.astype(np.float32))
 X_test = torch.from_numpy(X_test.astype(np.float32))
@@ -31,12 +28,12 @@ y_test = torch.from_numpy(y_test.astype(np.float32))
 y_train = y_train.view(y_train.shape[0], 1)
 y_test = y_test.view(y_test.shape[0], 1)
 
-# modelo
 
-class LogisticRegression(nn.Module):
-
+# 1) Model
+# Linear model f = wx + b , sigmoid at the end
+class Model(nn.Module):
     def __init__(self, n_input_features):
-        super(LogisticRegression, self).__init__()
+        super(Model, self).__init__()
         self.linear = nn.Linear(n_input_features, 1)
 
     def forward(self, x):
@@ -44,35 +41,32 @@ class LogisticRegression(nn.Module):
         return y_pred
 
 
-model = LogisticRegression(n_features)
+model = Model(n_features)
 
-# loss and optimizer
-learning_rate = 0.001
+# 2) Loss and optimizer
+num_epochs = 100
+learning_rate = 0.01
 criterion = nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-# loop de entrenamiento
+# 3) Training loop
+for epoch in range(num_epochs):
+    # Forward pass and loss
+    y_pred = model(X_train)
+    loss = criterion(y_pred, y_train)
 
-num_epocas = 1000
-for epoca in range(num_epocas):
-    # forward pass
-    y_predicted = model(X_train)
-    loss = criterion(y_predicted, y_train)
-
-    # backward pass
+    # Backward pass and update
     loss.backward()
-
-    # updates
     optimizer.step()
 
-    # zero gradients
+    # zero grad before new step
     optimizer.zero_grad()
 
-    if (epoca + 1) % 10 == 0:
-        print(f'epoca: {epoca+1}, loss = {loss.item():.4f}')
+    if (epoch + 1) % 10 == 0:
+        print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
 
 with torch.no_grad():
     y_predicted = model(X_test)
     y_predicted_cls = y_predicted.round()
     acc = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0])
-    print(f'accuracy = {acc:.4f}')
+    print(f'accuracy: {acc.item():.4f}')
